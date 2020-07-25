8f944db06dbe00f7ce36eacffe98c2bb2b06c346
Wilder H. Lizama Garcia <wilderlizama@gmail.com>
2020-07-25 12:18:15 -0500

    Estilo página registro

---
+++ S07/TareaCalificada-PC02/script.js
@@ -0,0 +1,18 @@
+
+function init() {
+
+    function onSubmit(evt) {
+        // var apellidos = document.getElementById("txtApellidos").value
+        // var nombres = document.getElementById("txtNombres").value
+
+        // if (apellidos.trim() === "" || nombres.trim() === "") {
+        //     evt.preventDefault()
+        //     alert("Faltan datos, por favor completar")
+        //     return
+        // }
+    }
+
+    document.getElementById("frmRegistro").addEventListener("submit", onSubmit)
+}
+
+window.addEventListener("load", init)
